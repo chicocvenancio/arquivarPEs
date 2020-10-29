94624d0a3b30ae851f5d9729bd194d48ca18c326
Chico Venancio <chicocvenancio@gmail.com>
2020-10-29 18:24:29 -0300

    Add Banner, exclude more unecessary polyfillys.
    Inspired by AC/DC configs by @lucaswerkmeister, available at
    https://github.com/lucaswerkmeister/ACDC

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,34 @@
+var webpack = require('webpack');
+
 module.exports = {
 	mode: 'production',
 	entry: './Gadget-arquivarPEs.js',
     target: ["web", "es5"],
+    plugins: [
+        new webpack.BannerPlugin( `
+ArquivarPEs - Arquivar Páginas a eliminar
+
+Adiciona um link (um "Arquivar" na linha de ferramentas da PE)
+para administradores e eliminadores nas páginas para eliminar
+que faz as ações de fechamento automaticamente.
+
+Essa versão foi construída com webpack e Babel.
+Podes encontrar o código original no GitHub:
+
+https://github.com/chicocvenancio/arquivarPEs
+
+Lá também é o aonde é feito o desenvolvimento,
+por favor não edite a essa página diretamente.
+
+This version was built with webpack and Babel.
+You can find the original source code on GitHub:
+
+https://github.com/chicocvenancio/arquivarPEs
+
+That is also where development happens –
+please do not edit this page directly.
+` ),
+    ],
 	module: {
 		rules: [
 			{
@@ -29,6 +56,19 @@ module.exports = {
 									corejs: '3',
 									exclude: [
 										'es.promise', // polyfilled via es6-promise ResourceLoader module instead
+                                        // supported everywhere, we don’t care about the subtle edge cases that core-js polyfills:
+										'es.array.filter',
+										'es.array.index-of', // not actually used, false positive from page.indexOf( '|' ) where page is a String, not an Array
+										'es.array.map',
+										'es.array.reduce',
+										'es.array.slice',
+										'es.array.some',
+										'es.array.splice',
+										'es.string.replace',
+										'es.string.split',
+                                        'es.regexp.exec',
+                                        'es.parse-int', // we always specify radix
+                                        'es.array.concat',
 
 									],
 								},
